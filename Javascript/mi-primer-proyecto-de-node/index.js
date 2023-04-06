191673bb8ba7ab38b4a3863c0ebf336760141e70
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-04-06 18:28:48 -0500

    Ejercicio en clase con VScode, Node y Npm

---
+++ Javascript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,15 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*
+
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los chequeos necesarios
+    Por último vamos a la última linea de saludo
+
+    Aquí tambien
+
+*/
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adiós")

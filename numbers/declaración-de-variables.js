c61badb37b65e501a45337e77550d8417b487a36
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-04-13 16:36:24 -0500

    Clase Js con numeros

---
+++ numbers/declaración-de-variables.js
@@ -0,0 +1,21 @@
+// Sesión 5 numbers
+// Declracion de variables numéricas (enteros y con decimales)
+
+let a = 5;
+
+console.log(a);
+
+let b = 0.1;
+
+console.log(b);
+
+//Precisión
+let c = 0.2;
+
+console.log(b + c);
+
+//Pequeño truco para obtener valores "rales"
+let d = Math.round((b + c) * 100) / 100;
+
+console.log(d)
+

6b944ee85204f3b66018da87a51819b5365c05e0
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-04-30 19:29:04 -0500

    Clase Js Configuración y uso de ESlint

---
+++ Javascript/eslint/eslint-npm/index.js
@@ -0,0 +1,26 @@
+// Este es el archivo que vamos a utilizar
+
+const nombre = "Alejo"
+
+
+/* eslint-disable*/
+
+const persona2 = 'Maria'
+
+/* eslint-enable*/
+
+// En esta linea quiero tener comillas simples (quiero que me desactives la regla de las comillas dobles)
+
+const persona3 = "Marcos"; // eslint-disable-line semi
+const persona4 = 'Mario' // eslint-disable-line quotes
+const persona5 = 'Marcelo'; // eslint-disable-line 
+
+/* eslint-disable-next-line */
+  const string = 'New string';
+
+/* eslint-disable-next-line indent */
+  const string1 = "Other string"
+
+const persona6 = "Julian" 
+console.log("Hola")
+console.log("Chao")
---
+++ Javascript/eslint/index.js
@@ -0,0 +1,11 @@
+const nombre = "Alejo"
+
+    var apellido = 'Lopez'; 
+
+    var objeto = {
+        nombre: "circulo",
+        radio: "2",
+        nombre: "Rectangulo"
+    }
+
+

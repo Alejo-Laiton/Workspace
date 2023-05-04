2c52958ef61be20a7a749fcb258e9b47d7dcc687
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-05-04 00:58:59 -0500

    Clase Js gestion de eventos y drag-drop

---
+++ Javascript/drag-drop/index.js
@@ -0,0 +1,51 @@
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+const papelera = document.querySelector(".papelera")
+
+parrafos.forEach(parrafo => {
+
+    parrafo.addEventListener("dragstart", event => {
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".imagen-fantasma")
+        event.dataTransfer.setDragImage(elemento_fantasma, 200, 30)
+    })
+
+    parrafo.addEventListener("dragend", () => {
+        // console.log("He terminado de arrastrar")
+        parrafo.classList.remove("dragging")
+    })
+}) 
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy"
+        // console.log("Drag Over")
+        //
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        // console.log("Parrafo id: ", id_parrafo)
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
+
+papelera.addEventListener("dragover", event => {
+    event.preventDefault()
+    papelera.classList.add("destino-valido")
+    event.dataTransfer.dropEffect = "copy"
+})
+
+papelera.addEventListener("drop", event => {
+    console.log("Delete")
+    const id_parrafo = event.dataTransfer.getData("id")
+    const parrafo = document.getElementById(id_parrafo)
+    parrafo.remove()
+    papelera.classList.remove("destino-valido")
+})
+
